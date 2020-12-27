0146e31448673ce23f88561aec545c7974011f0a
Alex <alex.perzhola@gmail.com>
2020-12-27 22:13:15 +0000

    Renamed all critical string according to SonarLint. Added overrided methods hashCode,clone,toString,equals to the Animal Class

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -7,39 +7,97 @@ public final class Animal {
     private final boolean _Vaccinated;
     private  final Passport _Passport;
 
-    public Animal(String species, int weight, int age, boolean Vaccinated,Passport Passport) {
+    public Animal(String species, int weight, int age, boolean vaccinated,Passport passport) {
         _Species = species;
         _Weight = weight;
         _Age = age;
-        _Vaccinated = Vaccinated;
+        _Vaccinated = vaccinated;
         _Passport = new Passport();
 
-        _Passport.PassportID = Passport.PassportID;
-        _Passport.OwnerFIO = Passport.OwnerFIO;
-        _Passport.AdditionalInfo = Passport.AdditionalInfo;
+        _Passport.PassportID = passport.PassportID;
+        _Passport.OwnerFIO = passport.OwnerFIO;
+        _Passport.AdditionalInfo = passport.AdditionalInfo;
     }
 
-    public String GetSpecies(){
+    public String getSpecies(){
         return this._Species;
     }
 
-    public  int GetWeight(){
+    public  int getWeight(){
         return  this._Weight;
     }
 
-    public  int GetAge(){
+    public  int getAge(){
         return  this._Age;
     }
 
-    public boolean IsVaccinated(){
+    public boolean isVaccinated(){
         return  this._Vaccinated;
     }
 
-    public  Passport GetPassport(){
+    public  Passport getPassport(){
         Passport pas = new Passport();
         pas.PassportID = _Passport.PassportID;
         pas.OwnerFIO = _Passport.OwnerFIO;
         pas.AdditionalInfo = _Passport.AdditionalInfo;
         return  pas;
+    }
+
+    @Override
+    protected Object clone() {
+        Animal animal = new Animal(this._Species,this._Weight,this._Age,this._Vaccinated,this.getPassport());
+        return animal;
+    }
+
+    @Override
+    public int hashCode() {
+        int val = 0;
+        val += this._Species.length();
+        val += this._Age;
+        val += this._Weight;
+        if(this._Vaccinated)
+            val += 1;
+        val += this._Passport.PassportID;
+        val += this._Passport.OwnerFIO.length();
+        val += this._Passport.AdditionalInfo.length();
+        return val % 14 + this._Species.length();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null)
+            return  false;
+        if(this == obj)
+            return  true;
+        if(this.getClass() != obj.getClass())
+            return false;
+
+        Animal anotherAnimal = (Animal)obj;
+
+        if (this._Species != anotherAnimal._Species)
+            return  false;
+        if(this._Age != anotherAnimal._Age)
+            return  false;
+        if(this._Weight != anotherAnimal._Weight)
+            return  false;
+        if(this._Vaccinated != anotherAnimal._Vaccinated)
+            return  false;
+
+        Passport firstPassport = this._Passport;
+        Passport secondPassport = anotherAnimal._Passport;
+
+        if(firstPassport.PassportID != secondPassport.PassportID)
+            return  false;
+        if(firstPassport.OwnerFIO != secondPassport.OwnerFIO)
+            return  false;
+        if(firstPassport.AdditionalInfo != secondPassport.AdditionalInfo)
+            return  false;
+
+        return  true;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal: " + this._Species + " Weight: " + this._Weight + " Age: " + this._Age + " Vaccinated: " + this._Vaccinated + " Passport Data: PassportID: " + this._Passport.PassportID + " AnimalOwner: " + this._Passport.OwnerFIO + " AdditionalInfo: " + this._Passport.AdditionalInfo + " EndPasswordData";
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	 Passport passport = new Passport();
+	 passport.PassportID = 1;
+	 passport.OwnerFIO = "John Bakery";
+	 passport.AdditionalInfo = "Some details";
+
+	 Animal animal = new Animal("Dog",15,10,true,passport);
+	 System.out.println(animal.toString());
+	 System.out.println(animal.hashCode());
+	 Animal animal2 = (Animal)animal.clone();
+	 System.out.println(animal.equals(animal2));
     }
 }
